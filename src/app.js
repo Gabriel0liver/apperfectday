6cde2ad9b65e2b5fc823fa6bb7946aa844a98f70
Gabriel Oliver <gabriel.oliver77@gmail.com>
2023-03-05 21:38:35 +0100

    puerto

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use("/calendar", calendarRouter);
 
 const port = config.port;
 
-app.listen(80, (err) => {
+app.listen(8080, (err) => {
   if (err) {
     console.error("No se pudo inicializar el servidor: " + err.message);
   } else {
