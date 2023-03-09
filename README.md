ee9eeb54658efb744adc624b6d95889a71ab52d3
aroy92 <andoni.roy@gmail.com>
2023-03-09 15:38:11 +0100

    Añadidos nconf y los entornos local, dev y prod

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # apperfectday
+
+Arrancar aplicación:
+
+- Local:
+
+La primera vez usa: `npm i -g nodemon`
+
+Para arrancar la aplicación: `npm start`
+
+- Development:
+
+Para arrancar la aplicación: `npm run start:dev`
+
+- Production:
+
+Para arrancar la aplicación: `npm run start:prod`
--- src/app.js
+++ src/app.js
@@ -1,33 +1,55 @@
 "use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const path = require("path");
 const express = require("express");
 const indexRouter = require("./routes/index");
 const calendarRouter = require("./routes/calendar");
+const nconf = require("nconf");
+const { existsSync } = require("fs");
 
-//Renderizar pagina
-const app = express();
-app.set("view engine", "ejs");
-app.set("views", path.resolve("src/views"));
+(() => {
+  let env = process.env.NODE_ENV || "local";
 
-//req.body
-
-app.use(express.urlencoded({ extended: false }));
-
-//fichero estaticos
-app.use(express.static(path.resolve("public")));
-
-app.use("/", indexRouter);
-app.use("/calendar", calendarRouter);
-
-const port = process.env.PORT;
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("No se pudo inicializar el servidor: " + err.message);
-  } else {
-    console.log(`Servidor arrancado en el puerto ${port}`);
+  const envs = ["production", "development", "local"];
+  if (envs.indexOf(env) == -1) {
+    console.log(
+      `${env} no es un entorno disponible. Asignado el entorno "local"`
+    );
+    env = "local";
   }
-});
+
+  const nconfPath = `nconf/${env}.json`;
+
+  if (!existsSync(nconfPath)) {
+    console.error(`No se encontró el fichero nconf/${env}.json`);
+    return;
+  }
+  nconf.file("global", { file: nconfPath });
+
+  //Renderizar pagina
+  const app = express();
+  app.set("view engine", "ejs");
+  app.set("views", path.resolve("src/views"));
+
+  //req.body
+
+  app.use(express.urlencoded({ extended: false }));
+
+  //fichero estaticos
+  app.use(express.static(path.resolve("public")));
+
+  app.use("/", indexRouter);
+  app.use("/calendar", calendarRouter);
+
+  const port = nconf.get("server:port");
+
+  app.listen(port, (err) => {
+    if (err) {
+      console.error("No se pudo inicializar el servidor: " + err.message);
+    } else {
+      console.log(`Servidor arrancado en el puerto ${port}`);
+    }
+  });
+})();
